6b84c7bb122ed6b247f037324d413f871eecc9da
carlosjcuevasmpucmm <carlosjcuevasmpucmm@gmail.com>
2019-11-01 02:44:33 -0400

    Final modelado ordersPack Sueno, cuidado...

---
+++ app/api/models/ordersPack.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//Modelado de la clase OrdersPack
+const OrdersPack = new Schema({
+
+    //Creador de la lista. Asociado a un usuario.
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserModel',
+        required: true
+      },
+    
+    //Arreglo de ordenes, asociado al modelado de la clase Order.
+    Order: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderModel',
+      }],
+
+    //Fecha en que se concibio la entrada.
+    creationDate: {
+        type: Date,
+        default: Date.now()
+    },
+
+    //A ser usada para un temporizador de las lista de ordenes.
+    expirationDate: {
+      type: Date,
+      default: (Date.now() + 3500000)
+  
+    },
+});
+module.exports = mongoose.model('OrdersPackModel', OrdersPack)
