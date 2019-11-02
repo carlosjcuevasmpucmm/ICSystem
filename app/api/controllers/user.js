dbd4c7d77afd956073ba38fdc45a7c509cfe4ae6
carlosjcuevasmpucmm <carlosjcuevasmpucmm@gmail.com>
2019-11-01 21:43:40 -0400

    Instalado dependencia correo nodemailer creando controlador

--- app/api/controllers/user.js
+++ app/api/controllers/user.js
@@ -1,7 +1,7 @@
 const userModel = require('../models/user');
 const bcrypt = require('bcrypt'); 
 const jwt = require('jsonwebtoken');
-
+const nodeMailer = require('nodemailer');
 module.exports = {
 
  create: function (req, res, next) {
@@ -33,4 +33,17 @@ res.json({status:"error", message: "Invalid email/password!!!", data:null});
      }
     });
  },
+
+emailOrderpack:function(req,res, next){
+    let transporter = nodeMailer.createTransport({
+        host: 'smtp.zoho.com',
+        port: 465,
+        secure: true,  //true for 465 port, false for other ports
+        auth: {
+            user: 'carlosjcuevasmpucmm@pucmm.edu.do',
+            pass: 'password'
+        }
+    });
+}
+
 }
